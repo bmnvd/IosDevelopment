5f3bc4b88aeaeb80c80912ed70f8e300df9c6a9e
bmnvd <danbaimenov05@mail.ru>
2025-12-18 04:22:01 +0500

    Add files via upload

---
+++ Shop/ShoppingCart/ShoppingCart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppingCart
+//
+//  Created by Daniyal Baimenov on 13.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Shop/ShoppingCart/ShoppingCartTests/ShoppingCartTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingCartTests.swift
+//  ShoppingCartTests
+//
+//  Created by Daniyal Baimenov on 13.12.2025.
+//
+
+import Testing
+@testable import ShoppingCart
+
+struct ShoppingCartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
